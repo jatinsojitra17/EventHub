cd015a26773ea34f7e97b3d85ca7c301f7c87380
Jatin <jatinpatel59967@gmail.com>
2024-04-25 09:00:29 +0530

    M_25_3

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import styles from "./DashboardPage.module.css";
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import NextEvents from '../../components/NextEvents/NextEvents';
+import UpcomingEvents from '../../components/UpcomingEvents/UpcomingEvents';
+import styles from './DashboardPage.module.css';
 
-
-export default function DashboardPage({user}) {
-    const [makeRsvp,setMakeRsvp] = useState(false);
+export default function DashboardPage({ user }) {
+    const [makeRsvp, setMakeRsvp] = useState(false);
     const navigate = useNavigate();
 
+    function handleClick(e) {
+        e.preventDefault();
+        navigate('/create-event')
+    }
+
     return (
-        <main className ={styles.dashboardPage}>
+        <main className={styles.dashboardPage}>
             <div className={styles.dashboardContainer}>
                 <h1 className={styles.userName}>Welcome, {user.name.split(' ')[0]} 👋</h1>
-                <button className={styles.btn}>Create Event</button>
+                <button className={styles.btn} onClick={handleClick} >Create Event</button>
             </div>
+            <NextEvents user={user} makeRsvp={makeRsvp} />
+            <UpcomingEvents user={user} makeRsvp={makeRsvp} setMakeRsvp={setMakeRsvp} />
         </main>
     );
 }
